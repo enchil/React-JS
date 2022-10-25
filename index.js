3df79cf736fa5db80ce1a651d94bbca4a5395444
enchil <enchiliang@gmail.com>
2022-10-25 12:07:34 +0800

    index

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-document.getElementById('total').addEventListener('click', function(){
+document.getElementById('plus').addEventListener('click', function(){
     document.getElementById('total').innerText = 
     Number(document.getElementById('total').innerText) +1;
 })
 
+document.getElementById('minus').addEventListener('click', function(){
+    document.getElementById('total').innerText = 
+    Number(document.getElementById('total').innerText) -1;
+})
